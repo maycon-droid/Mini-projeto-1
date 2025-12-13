285f2845b18c3c8ad0d213d8b56bc2e0dcab6fc5
Maycon <deepplayr@gmail.com>
2025-12-13 10:53:47 -0300

    ajusta tamanho dos cards centrais e estiliza h2

--- script.js
+++ script.js
@@ -1,35 +1,77 @@
 const dados = [
     {
         id: 1,
-        título: "Reciclagem em casa",
+        titulo: "Reciclagem em casa",
         categoria: "reciclagem",
         data: "2025-05-12",
-        descrição: "separar corretamente os materiais recicláveis em casa.",
+        descricao: "Separar corretamente os materiais recicláveis em casa.",
         curtidas: 0
     }
     ,
     {
         id: 2,
-        título: "Economia de água",
+        titulo: "Economia de água",
         categoria: "Consumo Consciente",
         data: "2025-06-20",
-        descrição: "reduzir tempo no banho e consertar vazamentos.",
+        descricao: "Reduzir tempo no banho e consertar vazamentos.",
         curtidas: 2
     }
+    ,
+    {  
+        id: 3,
+        titulo: "Transporte público",
+        categoria: "Mobilidade Sustentável",
+        data: "2025-07-15",
+        descricao: "Optar por transporte público ou bicicleta em vez de carro.",
+        curtidas: 5
+    }
+    ,
+    {  
+        id: 4,
+        titulo: "Plantar árvores",
+        categoria: "Reflorestamento",
+        data: "2025-08-10",
+        descricao: "Participar de campanhas de plantio de árvores na comunidade.",
+        curtidas: 3
+    }
+    , 
+    {  
+        id: 5,
+        titulo: "Reduzir uso de plástico",
+        categoria: "Consumo Consciente",
+        data: "2025-09-05",
+        descricao: "Utilizar sacolas reutilizáveis e evitar produtos com excesso de embalagem plástica.",
+        curtidas: 4
+    }
 ]
+let indiceLeftCentro = 1;
+let indiceCentro = 2;
+let indiceRightCentro = 3;
+
 const container = document.getElementById('card-container');
 
 function renderizarCards(lista) {
     container.innerHTML = '';
 
-    lista.forEach(item => {
+    lista.forEach((item, index) => {
         const card = document.createElement('div');
-        card.className = 'card';
+        card.classList.add('card');
+
+        if(index === indiceLeftCentro) {
+            card.classList.add('left-central-card');
+        }
+        if(index === indiceCentro) {
+            card.classList.add('central-card');
+        }
+        if(index === indiceRightCentro) {
+            card.classList.add('right-central-card');
+        }
+        
         card.innerHTML = `
-            <h3>${item.título}</h3>
-            <p><b>Categoria:</b> ${item.categoria}i</p>
+            <h3>${item.titulo}</h3>
+            <p><b>Categoria:</b> ${item.categoria}</p>
             <p><b>Data:</b> ${item.data}</p>
-            <p>${item.descrição}</p>
+            <p>${item.descricao}</p>
             <button id="like-btn-${item.id}">Curtir (${item.curtidas})</button>
         `;
         container.appendChild(card);
